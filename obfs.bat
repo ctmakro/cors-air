369a90bcfabd3826963ceff8ca54145b243eddc5
ctmakro <ctmakro@gmail.com>
2019-05-16 05:23:20 +0800

    obfsed

---
+++ obfs.bat
@@ -0,0 +1,3 @@
+
+# npm install javascript-obfuscator -g
+javascript-obfuscator sorry.js --string-array-threshold 1.0 --string-array-encoding base64 --disable-console-output false --identifier-names-generator mangled
